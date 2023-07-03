20db96cf50f4ddc49cdf0969b230c1b8f3f5ffa1
rlaalsrl715 <minkik715@gmail.com>
2023-07-03 21:00:55 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/couponissuer/CouponIssuerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.couponissuer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CouponIssuerApplication
+
+fun main(args: Array<String>) {
+    runApplication<CouponIssuerApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/couponissuer/CouponIssuerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.couponissuer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CouponIssuerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
